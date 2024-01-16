cdf7f6acb607a9df4e082f3f0cdbc5e2600c04f0
Rahul Jain <rahulkj@gmail.com>
2024-01-16 16:26:11 -0600

    Updated to latest version

--- coffee/src/main/java/com/example/CoffeeApplication.java
+++ coffee/src/main/java/com/example/CoffeeApplication.java
@@ -2,11 +2,9 @@ package com.example;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
-@EnableCircuitBreaker
 @EnableDiscoveryClient
 public class CoffeeApplication {
 
--- coffee/src/main/java/com/example/service/SnacksService.java
+++ coffee/src/main/java/com/example/service/SnacksService.java
@@ -6,14 +6,11 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
-import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
-
 @Service
 public class SnacksService {
 	@Value("${snacks.app.url}")
 	private String snacksAppUrl;
 
-	@HystrixCommand(fallbackMethod = "defaultSnacksMenu")
 	public String getSnacksMenu() {
 		RestTemplate restTemplate = new RestTemplate();
 		URI uri = URI.create(snacksAppUrl + "/menu");
